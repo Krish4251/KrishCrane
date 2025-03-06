ad7ef07dc22e808fee129892b89e82205a4ca723
Krish4251 <12302080603005@adit.ac.in>
2025-03-06 14:35:20 +0530

    Initial commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 import about_img from '../../assets/About.jpg'
-import play_icon from '../../assets/play-icon.png'
+import play_icon from '../../assets/Play-icon.png'
 const About = ({setPlayState}) => {
   return (
     <div className='about'>
