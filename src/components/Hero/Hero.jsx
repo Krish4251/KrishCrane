6f3072413e52d48f681cd8652875747295653bc9
Krish4251 <12302080603005@adit.ac.in>
2025-03-07 14:12:44 +0530

    Crane Vector update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,16 @@ const Hero = () => {
   return (
     <div className='hero container'>
       <div className='hero-text'>
-        <h1>Precision. Power. Performance.</h1>
+        <h1>Precision. <br/>Power. <br/>Performance.</h1>
         <p>Leading the Way in Crane Manufacturing</p>
         <button className='btn'>Explore More</button>
+        
+        
+      </div>
+      <div className='crane-vectors'>
+        <img className='image' src="src\assets\CraneVectors\Girder.png" alt=""/>
+        <img className='image trolley' src="src/assets/CraneVectors/Trolley.svg" alt="" />
+        <img className='image' src="src\assets\CraneVectors\Goliath Crane.svg" alt="" />
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,13 +20,21 @@ const Navbar = () => {
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className='logo' />
+
         <ul className={mobileMenu?'':'hide-mobile-menu'}>
+
           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+
           <li><Link to='products' smooth={true} offset={-260} duration={500}>Products</Link></li>
+
           <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
+          
           <li><Link to='plant' smooth={true} offset={-260} duration={500}>Plant</Link></li>
+
           <li><Link to='contact' smooth={true} offset={-260} duration={500}><button className='btn'>Contact</button></Link></li>
+
         </ul>
+        
         <img src={menu_icon} alt="" className='menu-icon'onClick={toggleMenu}/>
     </nav>
   )
