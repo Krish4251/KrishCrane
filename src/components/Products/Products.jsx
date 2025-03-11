d47c071438d41e54152e5086222e7e827e85f106
Krish4251 <12302080603005@adit.ac.in>
2025-03-12 04:03:50 +0530

    Product Hover images

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,23 +3,29 @@ import "./Products.css";
 import Product_1 from "../../assets/Product_1.png";
 import Product_2 from "../../assets/Product_2.png";
 import Product_3 from "../../assets/Product_3.png";
+import productHover_1 from "../../assets/productHover_1.png";
+import productHover_2 from "../../assets/productHover_2.png";
+import productHover_3 from "../../assets/productHover_3.png";
 const Products = () => {
   return (
     <div className="products">
       <div className="product">
-        <img src={Product_1} alt="" />
+        <img className="base-image" src={Product_1} alt="" />
+        <img className="hover-image" src={productHover_1} alt="" />
         <div className="caption">
           <p>EOT Crane</p>
         </div>
       </div>
       <div className="product">
-        <img src={Product_2} alt="" />
+        <img className="base-image" src={Product_2} alt="" />
+        <img className="hover-image" src={productHover_2} alt="" />
         <div className="caption">
           <p>Crane Hoist</p>
         </div>
       </div>
       <div className="product">
-        <img src={Product_3} alt="" />
+        <img className="base-image" src={Product_3} alt="" />
+        <img className="hover-image" src={productHover_3} alt="" />
         <div className="caption">
           <p>Goliath Crane</p>
         </div>
